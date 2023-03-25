23dbc369ae6882c2a876db703081cba541140915
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-03-25 14:57:05 +0530

    upload and crop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
+// import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+// import { RecipeComponent } from './recipe/recipe.component';
+// import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'recipe/details/:id',component:RecipeDetailComponent},
+  // {path:'recipe',component:RecipeComponent},
+  // {path:'shopping',component:ShoppingListComponent},
+  // { path: '**', component: RecipeComponent, pathMatch: 'full' },
+  {path:'**',component:ImageUploadComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {CarouselModule} from 'primeng/carousel';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+// import { RecipeComponent } from './recipe/recipe.component';
+// import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+// import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+// import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+// import {MatTableModule} from '@angular/material/table';
+// import {MatIconModule} from '@angular/material/icon'
+// import {MatSelectModule} from '@angular/material/select';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatRadioModule} from '@angular/material/radio';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ImageUploadComponent } from './image-upload/image-upload.component'
+import { UploadDialogComponent } from './image-upload/upload-dialog/upload-dialog.component';
+import { ImageCropperModule } from 'ngx-image-cropper';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // RecipeComponent,
+    // RecipeDetailComponent,
+    UploadDialogComponent,
+    // ShoppingListComponent,
+    ImageUploadComponent
   ],
   imports: [
+    ImageCropperModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    // MatCardModule,
+    // MatTableModule,
+    // MatIconModule,
+    AppRoutingModule,
+    CarouselModule,
+    MatDialogModule,
+    // MatSelectModule,
+    ButtonModule,
+    MatRadioModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
